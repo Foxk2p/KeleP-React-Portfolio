8bd76ddf8621e8861b343f069413f99e77d76f0f
K2P <foxk2p@gmail.com>
2020-06-26 21:47:17 -0700

    contact page formatting

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -2,7 +2,52 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <h1>This is my contact info</h1>
+    <>
+      <div class="row">
+        <div class="col-sm">
+          <p class="titles">Contact Me
+          </p>
+        </div>
+      </div>
+
+      <div class="spacing-div"></div>
+
+
+      <div class="container">
+        <div class="row">
+          <div class="col-sm ">
+            <h2 class="titles titleBg ">
+              Contact me by email:
+            </h2>
+            <p class="marginLeft">
+              foxk2p@gmail.com
+            </p>
+          </div>
+
+          <div class="col-sm center">
+            <h2 class="titles titleBg ">
+              Contact me by phone:
+            </h2>
+            <p>
+              (808) 398-5798)
+            </p>
+          </div>
+        </div>
+
+        <div class="spacing-div-xl"></div>
+
+          {/* <div class="row">
+            <div class="col center">
+              <a href="./index.html">
+                <button>Back to Bio</button>
+              </a>
+            </div>
+          </div> */}
+
+      </div>
+
+        <div class="spacing-div-sm"></div>
+    </>
   )
 }
 
