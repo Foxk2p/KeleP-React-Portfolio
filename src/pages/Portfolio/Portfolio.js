a401b59e0e228190ceb42f5b94295edf3a9d0a97
K2P <foxk2p@gmail.com>
2020-06-27 11:59:57 -0700

    portfolio JSX formatting

--- src/pages/Portfolio/Portfolio.js
+++ src/pages/Portfolio/Portfolio.js
@@ -2,7 +2,116 @@ import React from 'react'
 
 const Portfolio = () => {
   return (
-    <h1>This is my portfolio</h1>
+    <>
+      <div className="spacing-div"></div>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-sm col-6">
+            <a href="https://github.com/IsaacVon/Project1-UrbanHomesteading-" target="_blank" />
+            <div className="titleBg">
+              <h5 className="titles">Urban Homesteading</h5>
+              <h6 className="subTitles">View Project Code</h6>
+            </div>
+            </a>
+          <a href="https://isaacvon.github.io/Project1-UrbanHomesteading-/" target="_blank">
+            <div className="titleBg">
+              <img src="./Assets/images/Urban Homesteading.png" alt="Urban Homesteading" className="img-thumbnail"
+                style="width: 400px; height: 300px;" />
+              <h6 className="subTitles">View Live Application</h6>
+            </div>
+          </a>
+        </div>
+
+        <div className="col-sm col-1"></div>
+
+        <div className="col-sm col-6">
+          <a href="https://github.com/Foxk2p/Project-2" target="_blank">
+            <div className="titleBg">
+              <h5 className="titles">Movie Blog</h5>
+              <h6 className="subTitles">View Project Code</h6>
+            </div>
+          </a>
+          <a href="https://salty-reef-54366.herokuapp.com/" target="_blank">
+            <div className="titleBg">
+              <img src="./Assets/images/Movie-Sense.png" alt="My-Work-Day-Scheduler" className="img-thumbnail"
+                style=" width: 400px; height: 300px;" />
+              <h6 className="subTitles">View Live Application</h6>
+            </div>
+          </a>
+        </div>
+      </div>
+
+      <div className="spacing-div"></div>
+
+      <div className="row">
+        <div className="col-sm  col-6">
+          <a href="https://github.com/Foxk2p/JavaScript-Code-Quiz" target="_blank">
+            <div className="titleBg">
+              <h5 className="titles">JavaScript Quiz</h5>
+              <h6 className="subTitles">View Project Code</h6>
+            </div>
+          </a>
+          <a href="https://foxk2p.github.io/JavaScript-Code-Quiz/" target="_blank">
+            <div className="titleBg">
+              <img src="./Assets/images/JavaScript-Quiz.png" alt="JavaScript-Quiz" className="img-thumbnail"
+                style=" width: 400px; height: 300px;" />
+              <h6 className="subTitles">View Live Application</h6>
+            </div>
+          </a>
+        </div>
+        <div className="col-sm col-1"></div>
+        <div className="col-sm  col-6">
+          <a href="https://github.com/Foxk2p/Weather-Dashboard" target="_blank">
+            <div className="titleBg">
+              <h5 className="titles">Weather Dashboard</h5>
+              <h6 className="subTitles">View Project Code</h6>
+            </div>
+          </a>
+          <a href="https://foxk2p.github.io/Weather-Dashboard/" target="_blank">
+            <div className="titleBg">
+              <img src="./Assets/images/Weather-Dashboard.png" alt="Weather-Dashboard" className="img-thumbnail"
+                style=" width: 400px; height: 300px;" />
+              <h6 className="subTitles">View Live Application</h6>
+            </div>
+          </a>
+        </div>
+      </div>
+
+      <div className="spacing-div"></div>
+
+      <div className="row">
+        <div className="col-sm col-6">
+          <a href="https://github.com/Foxk2p/My-Work-Day-Scheduler" target="_blank">
+            <div className="titleBg">
+              <h5 className="titles">My Work Day Scheduler
+                </h5>
+              <h6 className="subTitles">View Project Code
+                </h6>
+            </div>
+          </a>
+          <a href="https://foxk2p.github.io/My-Work-Day-Scheduler/" target="_blank">
+            <div className="titleBg">
+              <img src="./Assets/images/My-Work-Day-Scheduler.png" alt="My-Work-Day-Scheduler" className="img-thumbnail"
+                style=" width: 400px; height: 300px;" />
+              <h6 className="subTitles">View Live Application</h6>
+            </div>
+          </a>
+        </div>
+      </div>
+
+      <div className="col-sm col-1">
+      </div>
+      <div className="col-sm col-6">
+      </div>
+    </div>
+
+    <div class="spacing-div"></div>
+
+    <div class="spacing-div"></div>
+    </div >
+
+    </>
   )
 }
 
