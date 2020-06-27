b0848f8d03d292e70866fa246a8c521aa0d4e0bf
K2P <foxk2p@gmail.com>
2020-06-27 15:05:54 -0700

    Updates

---
+++ precache-manifest.cb2432607392377df663ed30d1c6f51b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17e293dadd44d1737c8c5533e18de882",
+    "url": "/KeleP-React-Portfolio/index.html"
+  },
+  {
+    "revision": "f2466abc0541341f0464",
+    "url": "/KeleP-React-Portfolio/static/css/main.48df5c43.chunk.css"
+  },
+  {
+    "revision": "9ddb22e588feb3ac37f3",
+    "url": "/KeleP-React-Portfolio/static/js/2.25ab81c2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/KeleP-React-Portfolio/static/js/2.25ab81c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2466abc0541341f0464",
+    "url": "/KeleP-React-Portfolio/static/js/main.87d4c8e5.chunk.js"
+  },
+  {
+    "revision": "6d1d019c138de79ee919",
+    "url": "/KeleP-React-Portfolio/static/js/runtime-main.dc725964.js"
+  },
+  {
+    "revision": "c27c21dc6cda2a117c3b6696d6fea529",
+    "url": "/KeleP-React-Portfolio/static/media/JavaScript-Quiz.c27c21dc.png"
+  },
+  {
+    "revision": "52b3d7c324bfb464bfa5f3b1d290d898",
+    "url": "/KeleP-React-Portfolio/static/media/Movie-Sense.52b3d7c3.png"
+  },
+  {
+    "revision": "bd4bbff7a42f75c9216e60b7904c27fd",
+    "url": "/KeleP-React-Portfolio/static/media/My-Work-Day-Scheduler.bd4bbff7.png"
+  },
+  {
+    "revision": "23450aaa68884336ba74e2a1b675ab68",
+    "url": "/KeleP-React-Portfolio/static/media/Urban Homesteading.23450aaa.png"
+  },
+  {
+    "revision": "b44888e41cb28bc07000fe68d0fce086",
+    "url": "/KeleP-React-Portfolio/static/media/Weather-Dashboard.b44888e4.png"
+  },
+  {
+    "revision": "95b06a74b1c9cc5ea78667e3409a7126",
+    "url": "/KeleP-React-Portfolio/static/media/kpBioPic2.95b06a74.png"
+  }
+]);
